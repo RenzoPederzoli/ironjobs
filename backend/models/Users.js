6865b62c3a543f380d25e7aa837dd34c76d6cb39
hararia <thedrew88@gmail.com>
2020-07-27 18:48:28 -0400

    added backend

---
+++ backend/models/Users.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema, model } = require("mongoose");
+
+// const PLM = require("passport-local-mongoose");
+
+const userSchema = new Schema(
+  {
+    email: String,
+    name: String,
+    googleId: String,
+    imageUrl: String,
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+// userSchema.plugin(PLM, { usernameField: "email" });
+
+module.exports = model("User", userSchema);
+
